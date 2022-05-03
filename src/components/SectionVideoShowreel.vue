734f399bf4464ee6dd3f1408e73c6dd1c7f5b71f
Maftei Cezar <cezar.maftei@gmail.com>
2022-05-03 13:31:24 +0300

    Added Showreel

--- src/components/SectionVideoShowreel.vue
+++ src/components/SectionVideoShowreel.vue
@@ -1,11 +1,17 @@
 <template>
   <section class="section-showreel">
-    <video poster="@/assets/images/img-placeholder-reel.jpg">
-      <source src="movie.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
+    <video
+      ref="portfolioShowreel"
+      preload
+      poster="@/assets/images/img-placeholder-reel.jpg"
+    >
+      <source
+        src="@/assets/videos/Webprofits_Portfolio_Showreel_1920x1080_220503.mp4"
+        type="video/mp4"
+      />
       Your browser does not support the video tag.
     </video>
-    <button class="btn btn-showreel">
+    <button @click="handleVideo($event)" class="btn btn-showreel">
       <SvgIcons icon="icon-play-text" />
     </button>
   </section>
@@ -13,36 +19,68 @@
 
 <script>
 import SvgIcons from '@/components/SvgIcons.vue'
+import { ref } from '@vue/reactivity'
 export default {
   name: 'SectionVideoShowreel',
   components: {
     SvgIcons
+  },
+  setup () {
+    const portfolioShowreel = ref(null)
+    // let isPlay = false
+
+    const handleVideo = (event) => {
+      // console.log(event.target)
+      event.target.closest('.btn').classList.add('d-none')
+      portfolioShowreel.value.classList.add('initialized')
+      portfolioShowreel.value.setAttribute('controls', 'true')
+      portfolioShowreel.value.play()
+      /*
+      if (isPlay) {
+        portfolioShowreel.value.pause()
+        isPlay = false
+      } else {
+        portfolioShowreel.value.classList.add('initialized')
+        portfolioShowreel.value.play()
+        isPlay = true
+      }
+      */
+    }
+
+    return {
+      handleVideo,
+      portfolioShowreel
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  .section-showreel {
-    position: relative;
-    height: 100vh;
+.section-showreel {
+  position: relative;
+  height: 100vh;
 
-    video {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    .btn-showreel {
-      margin: auto;
-      padding: 0;
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      fill: $white;
-      width: 216px;
-      height: 216px;
-    }
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+
+  video.initialized {
+    object-fit: contain;
+  }
+
+  .btn-showreel {
+    margin: auto;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    fill: $white;
+    width: 216px;
+    height: 216px;
+  }
+}
 </style>
