8eeeadd2af11930e6216952d254be2893036d22e
Maftei Cezar <cezar.maftei@gmail.com>
2022-05-06 07:58:52 +0300

    Update SectionVideoShowreel.vue

--- src/components/SectionVideoShowreel.vue
+++ src/components/SectionVideoShowreel.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="section-showreel">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/I8L84c-24X8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     <div class="showreel-video" id="showreel-video"></div>
     <button
       @click="playVideo"
